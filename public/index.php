cf2101b875776c4db8d13b8a3abc22b3d52382ec
vikabukach <bukachviko@gmail.com>
2024-12-29 11:43:40 +0200

    upgraded in index links

--- public/index.php
+++ public/index.php
@@ -19,10 +19,7 @@ $fg = 'dd'
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.tailwindcss.com"></script>
-
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <title>Family tree</title>
 </head>
 <body>
@@ -121,9 +118,7 @@ $fg = 'dd'
     <!------------LIST RELATIVES ---------------------->
 
 </main>
-
-
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-<script src="js/main.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
+<script src="/js/jquery.min.js"></script>
 </body>
 </html>
