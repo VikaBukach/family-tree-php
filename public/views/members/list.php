e8d16e4b46b46f89427fa8205b865da622d43bad
vikabukach <bukachviko@gmail.com>
2024-12-29 11:36:53 +0200

    upgraded links css, js

--- public/views/members/list.php
+++ public/views/members/list.php
@@ -15,9 +15,7 @@ $allRows = $db->getAllRows();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <!------------------LIST RELATIVES ---------------------->
@@ -79,7 +77,6 @@ $allRows = $db->getAllRows();
 </div>
 
 
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-<script src="js/main.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
