5dc1132676bf039fca10ded4595493484e208699
vikabukach <bukachviko@gmail.com>
2025-01-12 14:12:16 +0200

    added view relatetives in relationsform

--- public/views/members/relationsform.php
+++ public/views/members/relationsform.php
@@ -24,72 +24,104 @@ $allRoles = RoleRelationships::getAllRoles();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Cormorant+Unicase:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Cormorant+Unicase:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css"/>
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css"/>
     <!-- Or for RTL support -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.rtl.min.css" />
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.rtl.min.css"/>
+    <link rel="stylesheet" href="/css/styles.css">
 
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <link rel="stylesheet" href="/css/styles.css">
+
 
 </head>
 <body>
 <div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-xl-6 col-lg-6 mt-5 mb-5">
-            <h1 class="text-center fs-4 mt-2 mb-4 t-h1">Форма додавання відносин</h1>
+            <h1 class="text-center mb-4 mt-4">Форма додавання відносин</h1>
 
-            <form action="/controllers/RelationshipsController.php?action=createRelation" method="POST">
+            <form id="relations-form" action="/controllers/RelationshipsController.php?action=createRelation"
+                  method="POST">
 
                 <div class="mb-3">
-                <label for="member_id" class="t-h1">Член родини:</label>
-                <select name="member_id" class="form-select mt-3 mb-3" id="member-id" aria-label="Default select example">
-                    <option value="<?=$member['id'] ?>" selected><?=$member['name'] .' '. $member['surname']?></option>
-                </select>
+                    <label for="member_id" class="t-h1">Член родини:</label>
+                    <select name="member_id" class="form-select mt-3 mb-3" id="member-id"
+                            aria-label="Default select example">
+                        <option value="<?= $member['id'] ?>"
+                                selected><?= $member['name'] . ' ' . $member['surname'] ?></option>
+                    </select>
                 </div>
 
                 <div class="mb-3">
-                <label for="related_member_id" class="t-h1">Повʼязаний член родини:</label>
-                <select name="related_member_id" id="related_member_id" class="form-select mt-3 mb-3" aria-label="Default select example">
-                    <option selected value="">Oберіть іншого члена родини щоб повʼязати звязок:</option>
+                    <label for="related_member_id" class="t-h1">Повʼязаний член родини:</label>
+                    <select name="related_member_id" id="related_member_id" class="form-select mt-3 mb-3"
+                            aria-label="Default select example">
+                        <option selected value="">Oберіть іншого члена родини щоб повʼязати звязок:</option>
 
-                    <?php foreach ($allRows as $row) : ?>
+                        <?php foreach ($allRows as $row) : ?>
 
-                    <option value="<?=$row['id'] ?>"><?=$row['name'] .' '. $row['surname']?></option>
+                            <option value="<?= $row['id'] ?>"><?= $row['name'] . ' ' . $row['surname'] ?></option>
 
-                    <?php endforeach; ?>
-                </select>
+                        <?php endforeach; ?>
+                    </select>
                 </div>
 
                 <div class="mb-3">
-                <label for="relationship_type" class="t-h1">Тип звʼязку(роль):</label>
-                <select name="relationship_type" id="relationship_type" class="form-select mt-3 mb-3" aria-label="Default select example">
+                    <label for="relationship_type" class="t-h1">Тип звʼязку(роль):</label>
+                    <select name="relationship_type" id="relationship_type" class="form-select mt-3 mb-3"
+                            aria-label="Default select example">
 
-                    <option selected value="">Oберіть роль:</option>
+                        <option selected value="">Oберіть роль:</option>
 
-                    <?php foreach (RoleRelationships::getAllRoles() as $roleKey => $roleName): ?>
+                        <?php foreach (RoleRelationships::getAllRoles() as $roleKey => $roleName): ?>
 
-                    <option value="<?= $roleKey?>"><?= $roleName?></option>
+                            <option value="<?= $roleKey ?>"><?= $roleName ?></option>
 
-                  <?php endforeach; ?>
-                </select>
+                        <?php endforeach; ?>
+                    </select>
                 </div>
 
                 <div class="d-grid gap-2 col-6 mx-auto mt-3">
-                    <button type="submit" class="btn btn-outline-primar btn-lg">Зберегти</button>
+                    <button type="submit" class="btn btn-outline-primar btn-lg mt-3">Зберегти</button>
                 </div>
             </form>
+        </div>
+    </div>
+    <div class="container">
+        <h2 class="text-center mb-4 mt-4">Родинні зв’язки</h2>
+        <div class="relationships" style="display: flex; flex-wrap: wrap; gap: 20px; justify-content: center;">
 
-            <a class="d-grid gap-2 col-6 mx-auto mt-3 btn btn-outline-primar btn-lg" href="/">
-                Повернутись на головну
-            </a>
+            <ul class="card-container" style="display: flex; flex-wrap: wrap; gap: 20px; justify-content: center;">
+
+
+                <li class="card"
+                    style="width: 150px; height: 180px; border: 1px solid #ddd; border-radius: 8px; overflow: hidden; display: flex; flex-direction: column; align-items: center; padding: 5px;">
+                    <img src="" alt="фото"
+                         style="width: 100px; height: 100px; object-fit: cover; border-radius: 50%; margin-bottom: 10px;">
+                    <h6 style="margin-bottom: 5px; display: flex; text-align: center;">
+
+                    </h6>
+                    <p style="font-size: 14px; color: #555;">
+
+                    </p>
+
+                </li>
+
+            </ul>
+
+        </div>
+        <div class="d-grid gap-2 col-6 mx-auto mt-3">
+            <a class="btn btn-outline-primar btn-lg" href="/">Повернутись на головну</a>
         </div>
     </div>
 </div>
@@ -106,6 +138,19 @@ $allRoles = RoleRelationships::getAllRoles();
     });
 
 </script>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+<script>
+    $('#relations-form').on('submit', function (e) {
+        e.preventDefault();
+
+        var relatType = $('#relationship_type').val();
+        console.log(relatType)
+
+    })
+
+</script>
+
 </body>
 </html>
 
