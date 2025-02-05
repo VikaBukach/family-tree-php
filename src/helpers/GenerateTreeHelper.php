2f525a7b60636266309244956fb5fb856cbc8fc1
vikabukach <bukachviko@gmail.com>
2025-02-05 14:23:28 +0200

    change DEPTH_VALUE on= 4;

--- src/helpers/GenerateTreeHelper.php
+++ src/helpers/GenerateTreeHelper.php
@@ -6,7 +6,7 @@ use FamilyTree\entities\FamilyMember;
 
 class GenerateTreeHelper
 {
-    private const DEPTH_VALUE = 3;
+    private const DEPTH_VALUE = 4;
 
     private static $membersForTree = [];
     private static $currentCircle = 0;
